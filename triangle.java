0b8a485df19ef94f7079386440d1e9af312dbba3
mdkhairuddin90 <mdkhairuddin909@gmail.com>
2025-09-18 09:58:41 +0600

    Add files via upload

---
+++ triangle.java
@@ -0,0 +1,9 @@
+public class triangle {
+    public static void main(String args []){
+        float land=20;
+        double height=20.5;
+        double  result;
+        result = 0.5*land*height;
+        System.out.println("the triangle is = " +result);
+ }
+}
