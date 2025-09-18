af303f27c7a1eb7d28299168dd7e970ba4e0a340
mdkhairuddin90 <mdkhairuddin909@gmail.com>
2025-09-18 09:45:30 +0600

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,8 @@
+class arithmatic {
+
+    public static void main(String[] args) {
+        int a=20,b=40;
+        int result= a+b;
+        System.out.println("The result is = "+result);
+    }
+}
